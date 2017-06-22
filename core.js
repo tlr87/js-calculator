92def3681949c8778d2bb54cc85bbb7a0c9740f2
Tom Revill <tom.revill@gmail.com>
2017-06-23 11:49:26 +1200

    Code clean up

--- core.js
+++ core.js
@@ -6,25 +6,21 @@ var memoryBank = 0;
 var OppSwitch = "";
 var VarSwitch = false;
 var KeyCount = 0;
-var v = 0;
+var keyVal = 0;
 var imputScreen = [];
 var screenTemp = 0;
 
 //Keypad functions.
-function num(v) {
-  console.log('VarSwitch is ' + VarSwitch);
-  imputScreen.push(v);
+function num(keyVal) {
+  imputScreen.push(keyVal);
   var screenTemp = imputScreen.join('');
   var number = Number(screenTemp);
   document.getElementsByClassName("screen")[0].innerHTML = screenTemp;
   if (VarSwitch === false) {
     value_one = number;
-    console.log("VarSwitch" + value_one );
   } else if (VarSwitch === true) {
     value_two = number;
-    console.log("VarSwitch" + value_two );
   }
-
 }
 
 function PlusFn() {
@@ -79,26 +75,11 @@ function printAnswer() {
 function OnEqualClicked() {
   Answer();
   printAnswer();
-  console.log(imputScreen.length = 0);
-  value_one = 0;
-  value_two = 0;
-  Opp = "";
-  OppSwitch = "";
-  VarSwitch = "aVar";
-  KeyCount = 0;
-  v = 0;
+  timerClear = setTimeout(clearVariables, 4000);
 }
 
 
 function clearVariables() {
-  value_one = 0;
-  value_two = 0;
-  Opp = "";
-  memoryBank = 0;
-  OppSwitch = "";
-  VarSwitch = "aVar";
-  KeyCount = 0;
-  v = 0;
   console.log(imputScreen.length = 0);
   console.log(document.getElementsByClassName("screen")[0].innerHTML = '');
 }
